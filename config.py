7c92e922cff069fab37a75a25a4916e4b7d0f84c
Shu Ramakrishnan <124190672+shutubee@users.noreply.github.com>
2026-07-14 19:43:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+APP_NAME = "Kitchen Shelf Elastic Stability App"
+APP_VERSION = "0.2.0"
+
+DEFAULT_REFERENCE_PIXELS = 370.0
+DEFAULT_REFERENCE_LENGTH_M = 1.8
+
+DEFAULT_SECTION_SHAPE = "rectangular"
+DEFAULT_SECTION_PARAMS = {
+    "width_m": 0.02,
+    "depth_m": 0.02,
+}
+
+DEFAULT_K = 1.0
+DEFAULT_IMPERFECTION_MM = 2.0
+DEFAULT_AXIAL_LOAD_N = 600.0
+DEFAULT_ECCENTRICITY_MM = 5.0
+DEFAULT_EXTRA_MOMENT_NM = 5.0
+
+RISK_COLORS = {
+    "green": "green",
+    "amber": "orange",
+    "red": "red",
+}
